8eaca52474849e49768efbcf97958cbd44efd5c4
Guilherme Heynemann Bruzzi <guibruzzi@gmail.com>
2012-12-08 10:47:48 -0200

    Migration executada com sucesso

--- bands/migration20121128.py
+++ bands/migration20121128.py
@@ -11,24 +11,28 @@ def run_migration():
     questions = Question.objects.all()
 
     user_eh_musico = {
-        u"Camille Cardoso": "",
-        u"Aline Lucena Nascentes": "",
-        u"Bernardo Heynemann": "",
-        u"Guto Marrara Marzagao": "",
-        u"Guilherme Heynemann Bruzzi": "",
-        u"Mariana Marzagao": "",
-        u"Irene Heynemann": "",
-        u"André Sterenberg Frankenthal": "",
-        u"Hélio Cardoso Junior": "",
-        u"Michelle Andreu": "",
+        u"Camille Cardoso": "fa",
+        u"Aline Lucena Nascentes": "fa",
+        u"Bernardo Heynemann": "fa", # Nao preencheu
+        u"Guto Marrara Marzagao": "fa",
+        u"Guilherme Heynemann Bruzzi": "fa",
+        u"Mariana Marzagao": "fa",
+        u"Irene Heynemann": "fa",
+        u"André Sterenberg Frankenthal": "fa",
+        u"Hélio Cardoso Junior": "fa",
+        u"Michelle Andreu": "fa",
         u"Pedro Hollanda": "musico",
         u"Rodrigo de Toledo": "fa",
         u"Rafael Lepritiê Werneck": "fa",
         u"Daniel Silli Trevizan": "musico",
         u"Nando Medeiros": "musico",
-        u"Eduardo Neves": "",
-        u"Guilherme Gualter": "",
-    }
+        u"Eduardo Neves": "fa", # Nao preencheu
+        u"Guilherme Gualter": "fa",
+        u"Victor Fernandes Filho": "fa",
+        u"Douglas Quintanilha": "fa",
+        u"Felipe Botareli": "fa"
+    } # fa: 17, musico: 3
+
 
     question_slug = {
         u"Quais as suas bandas ou músicos favoritos?": "favoritos",
@@ -42,16 +46,6 @@ def run_migration():
 
     for question in questions:
         if question.question == u"Você é músico?":
-           for answer in question.answers:
-               if answer.answer == "musico":
-                   user_eh_musico[answer.user.name] = "musico"
-               else:
-                   user_eh_musico[answer.user.name] = "fa"
-
-    import ipdb; ipdb.set_trace()
-
-    for question in questions:
-        if question.question == u"Você é músico?":
             continue
 
         for answer in question.answers:
@@ -59,18 +53,22 @@ def run_migration():
             slug = "%s-%s" % (user_eh_musico[user.name], question_slug[question.question])
             slug_answers[slug].append(answer)
 
-#    question_main = get_or_create_question({"question": u"Você é músico?", "slug": "musico-ou-fa"} )
-#    answers_main = []
-#    for user_name, answer in user_eh_musico.items():
-#        for user in users:
-#            if user.name == user_name:
-#                answer_instance = Answer(answer=answer, user=user)
-#                answers_main.append(answer_instance)
-#    question_main.answers = answers_main
-#    question_main.save()
-#
-#    for question_pesquisa in QUESTIONS_PESQUISA:
-#        slug = question_pesquisa["slug"]
-#        question = get_or_create_question({"question": question_pesquisa["question"], "slug": slug})
-#        question.answers = slug_answers[slug]
-#        question.save()
+    question_main = get_or_create_question({"question": u"Você é músico?", "slug": "musico-ou-fa"} )
+    answers_main = []
+    for user_name, answer in user_eh_musico.items():
+        for user in users:
+            if user.name == user_name:
+                answer_instance = Answer(answer=answer, user=user)
+                answers_main.append(answer_instance)
+    question_main.answers = answers_main
+
+
+    question_main.save()
+
+    for question_pesquisa in QUESTIONS_PESQUISA:
+        slug = question_pesquisa["slug"]
+        question = get_or_create_question({"question": question_pesquisa["question"], "slug": slug})
+        question.answers = slug_answers[slug]
+
+
+        question.save()
