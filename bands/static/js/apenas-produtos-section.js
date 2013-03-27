222f2d7b98692eb32d03a1ad718ac53cd3025014
Guto <gutomarzagao@gmail.com>
2013-03-27 15:28:24 -0300

    diminuindo width do iframe

--- bands/static/js/apenas-produtos-section.js
+++ bands/static/js/apenas-produtos-section.js
@@ -16,9 +16,9 @@ function mainApenasProdutosSection(){
         var produtosSection = document.querySelector('.produtos-section');
         var areaBanda = document.querySelector('.area-banda');
         produtosSection.removeChild(areaBanda);
-        produtosSection.style.width = '780px';
+        produtosSection.style.width = '700px';
         produtosSection.style.margin = "0 0";
-        produtosSection.innerHTML += '<footer style="width: 780px; margin: 50px 0 30px 0;">Quer saber de shows e produtos de outras bandas? Acesse o <a target="_blank" href="http://bands.com.br"><img src="http://bands.com.br/static/img/logoBands.jpg" style="width: 64px; height: 50px; position: relative; top: 10px;"></a></footer>';
+        produtosSection.innerHTML += '<footer style="width: 700px; margin: 50px 0 30px 0;">Quer saber de shows e produtos de outras bandas? Acesse o <a target="_blank" href="http://bands.com.br"><img src="http://bands.com.br/static/img/logoBands.jpg" style="width: 64px; height: 50px; position: relative; top: 10px;"></a></footer>';
 
         window.setInterval(hideUserVoice, 500);
     }
