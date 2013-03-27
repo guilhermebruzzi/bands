762e961908b92da45036f736347d6e8f78485d58
Guto <gutomarzagao@gmail.com>
2013-03-27 15:24:01 -0300

    removendo margem do conteudo do iframe

--- bands/static/js/apenas-produtos-section.js
+++ bands/static/js/apenas-produtos-section.js
@@ -16,13 +16,12 @@ function mainApenasProdutosSection(){
         var produtosSection = document.querySelector('.produtos-section');
         var areaBanda = document.querySelector('.area-banda');
         produtosSection.removeChild(areaBanda);
-        produtosSection.style.width = '510px';
-        produtosSection.innerHTML += '<footer style="width: 510px">Quer saber de shows e produtos de mais bandas? Acesse o <a target="_blank" href="http://bands.com.br"><img src="http://bands.com.br/static/img/logoBands.jpg" style="width: 64px; height: 50px; position: relative; top: 10px;"></a></footer>';
+        produtosSection.style.width = '780px';
+        produtosSection.style.margin = "0 0";
+        produtosSection.innerHTML += '<footer style="width: 780px; margin: 50px 0 30px 0;">Quer saber de shows e produtos de outras bandas? Acesse o <a target="_blank" href="http://bands.com.br"><img src="http://bands.com.br/static/img/logoBands.jpg" style="width: 64px; height: 50px; position: relative; top: 10px;"></a></footer>';
 
         window.setInterval(hideUserVoice, 500);
     }
-    produtosSection.style.margin = "0 0";
-    produtosSection.style.width = "780px";
 }
 
 mainApenasProdutosSection();
